393b3e2443889183a3f7528fc342de59547ec443
Doğanay Bozkurt <sdoganay4@hotmail.com>
2025-09-03 16:22:35 +0300

    update py_01 , py_04

--- py_01/main.py
+++ py_01/main.py
@@ -1,10 +1,16 @@
 import pandas as pd
+from pandas.io.formats.format import return_docstring
 
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
-new_list = {row.letter:row.code for (index, row) in df.iterrows()}
+new_dic = {row.letter:row.code for (index, row) in df.iterrows()}
 
-input_name = input("Enter the name: ").upper()
-input_dic = [new_list[letter] for letter in input_name]
-print(input_dic)
-
+while True:
+    try:
+        input_name = input("Enter the name: ").upper()
+        input_list = [new_dic[letter] for letter in input_name]
+    except KeyError as e:
+        print(f'Error: {KeyError}, Only letters in the alphabet please.')
+    else:
+        print(input_list)
+        break
--- py_04/main.py
+++ py_04/main.py
@@ -1,6 +1,5 @@
 from random import shuffle, randint
 from tkinter import *
-import random
 from tkinter import messagebox
 import pandas as pd
 from PIL import ImageTk, Image
@@ -13,6 +12,21 @@ window = Tk()
 window.title("My Password Saver")
 window.geometry("750x600")
 window.configure(padx=10, pady=10)
+
+def info():
+    df = pd.read_csv("data.csv")
+    user_website = ent_website.get()
+    if user_website == "":
+        messagebox.showerror("Error", "Please enter a valid website")
+        return
+    for index, row in df.iterrows():
+        if user_website.lower().strip() == row[" --Name-- "]:
+            web = df.iloc[index, 0]
+            email = df.iloc[index, 1]
+            pwd = df.iloc[index, 2]
+            messagebox.showinfo(web, f"Email: {email}\nPassword: {pwd}")
+            return
+    messagebox.showerror("Error", f"this website {ent_website.get()} doesn't exist")
 
 def random_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -50,7 +64,7 @@ def save_data():
                                               f"\nPassword: {pwd_data}\n is it ok to save?")
         if check:
             df.loc[len(df)] = [web_data, user_data, pwd_data]
-            df.to_csv(save_files, sep="|", index=False)
+            df.to_csv(save_files, index=False)
             messagebox.showinfo("Success", "Data saved")
         else:
             messagebox.showinfo("Success", "Data not saved")
@@ -81,20 +95,25 @@ lbl_password = Label(window, text="Password:", font=("Arial", 16))
 lbl_password.grid(row=3, column=0,padx = 10, pady = 5,)
 
 #/-- enter --/
-ent_website = Entry(window, width=53, font=("Arial", 12))
-ent_website.grid(row=1, column=1, columnspan=2, pady = 5)
+ent_website = Entry(window, width=40, font=("Arial", 12))
+ent_website.grid(row=1, column=1, pady = 5)
 ent_website.focus()
+
 ent_user = Entry(window, width=53, font=("Arial", 12))
 ent_user.grid(row=2, column=1, columnspan=2, pady = 5)
 ent_user.insert(0, "@gmail.com")
+
 ent_password = Entry(window,width=40, font=("Arial", 12))
 ent_password.grid(row=3, column=1 , pady = 5)
 
 #/-- button --/
-btn_generate_password = Button(window, text="Generate Password", command=random_password)
+btn_search = Button(window, text="Search",width=14, bg='grey', command=info)
+btn_search.grid(row=1, column=2, pady = 5)
+
+btn_generate_password = Button(window, text="Generate Password", command=random_password, bg='grey')
 btn_generate_password.grid(row=3, column=2, pady = 5)
 
-btn_add = Button(window, text="Add", width=68, command=save_data)
+btn_add = Button(window, text="Add", width=68, command=save_data,bg='grey')
 btn_add.grid(row=4, column=2, pady = 5)
 btn_add.grid(row=4, column=1, columnspan=2, pady = 5)
 
