c179308ae2af4d2e720040468288f2ceaafe4869
Doğanay Bozkurt <sdoganay4@hotmail.com>
2025-09-04 18:04:00 +0300

    finished py_05

--- py_05/main.py
+++ py_05/main.py
@@ -5,25 +5,38 @@ import time
 
 BG_COLOR = "#B1DDC6"
 FIlE_NAME = "french_words.csv"
-
-df = pd.read_csv(FIlE_NAME)
-new_dict = df.to_dict(orient="records")
+NEW_WORDS_FILE = "check_words.csv"
+current_card = {}
+new_dict = {}
+try:
+    df = pd.read_csv(NEW_WORDS_FILE)
+except FileNotFoundError:
+    original_data = pd.read_csv(FIlE_NAME)
+    print(original_data)
+    new_dict = original_data.to_dict('records')
+else:
+    new_dict = df.to_dict(orient="records")
 
 
 def front_card():
-    global set_timer
+    global set_timer, current_card
     window.after_cancel(set_timer)
-    random_fr = random.choice(new_dict)
+    current_card = random.choice(new_dict)
     canvas.itemconfig(cnv_img, image=card_front_img)
     canvas.itemconfig(text_title, text="French", fill="black")
-    canvas.itemconfig(text_word, text=random_fr["French"], fill="black")
+    canvas.itemconfig(text_word, text=current_card["French"], fill="black")
     set_timer = window.after(3000, back_card)
 
 def back_card():
     canvas.itemconfig(cnv_img, image=card_back_img)
-    random_en = random.choice(new_dict)
     canvas.itemconfig(text_title, text="English", fill="white")
-    canvas.itemconfig(text_word, text=random_en["English"], fill="white")
+    canvas.itemconfig(text_word, text=current_card["English"], fill="white")
+
+def is_ok():
+    new_dict.remove(current_card)
+    new_data = pd.DataFrame(new_dict)
+    new_data.to_csv(NEW_WORDS_FILE, index=False)
+    front_card()
 
 window = Tk()
 window.title("Project")
@@ -40,12 +53,12 @@ text_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"),
 text_word = canvas.create_text(400, 253, text="", font=("Arial", 60, "bold"), fill="black")
 
 
-Ture_img = PhotoImage(file="./images/right.png")
-btn_True = Button(window, image=Ture_img, highlightthickness=0,command=front_card)
-btn_True.grid(row=1, column=1)
 False_img = PhotoImage(file="./images/wrong.png")
 btn_False = Button(window, image=False_img, highlightthickness=0,command=front_card)
 btn_False.grid(row=1, column=0)
+Ture_img = PhotoImage(file="./images/right.png")
+btn_True = Button(window, image=Ture_img, highlightthickness=0,command=is_ok)
+btn_True.grid(row=1, column=1)
 
 front_card()
 window.mainloop()
